c8a602d47f67b814f3425cf331a5f17e56fe498c
themavathi.mca@gmail.com <themavathi.mca@gmail.com>
2023-05-18 07:42:41 +0000

    Packages added

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,5 +1,6 @@
 select
 --from raw_orders
+{{ dbt_utils.generate_surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as sk_orders,
 orderid,
 orderdate,
 shipdate,
@@ -23,5 +24,5 @@ left join {{ ref('raw_customer') }} as c
 on o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p
 on o.productid = p.productid
-
-{{limit_data_in_dev('orderdate')}}
+--where o.orderdate >= dateadd('day', -30, current_timestamp)
+--{{limit_data_in_dev('orderdate')}}
