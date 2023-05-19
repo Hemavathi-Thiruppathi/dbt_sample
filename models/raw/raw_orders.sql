01b8f647d32499faf7f6f3b6e7e3038483cae08f
themavathi.mca@gmail.com <themavathi.mca@gmail.com>
2023-05-19 07:12:53 +0000

    Incremental option included

--- models/raw/raw_orders.sql
+++ models/raw/raw_orders.sql
@@ -1,2 +1,16 @@
+{{
+    config(
+        materialized='incremental',
+        unique_key='orderid'
+    )
+}}
+
 select * from 
 {{ source('globalmart', 'orders') }}
+
+{% if is_incremental() %}
+
+  -- this filter will only be applied on an incremental run
+  where update_dt > (select max(update_dt) from {{ this }})
+
+{% endif %}
